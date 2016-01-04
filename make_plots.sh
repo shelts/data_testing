e6a127c0a5a242a31a70f37cd250e9c144e3d9ca
Sidd <shelts@rpi.edu>
2016-01-04 18:25:51 -0500

    update

--- make_plots.sh
+++ make_plots.sh
@@ -3,9 +3,10 @@
 
 
     cd gnuplot_scripts/
-    ./stability_plots.py
+#     ./stability_plots.py
     ./multiplot_stability_plots.py
-#     cd ..
+    
+
 #     gnuplot stability_rad.gnuplot
 #     gnuplot stability_phi.gnuplot
 #     gnuplot stability_theta.gnuplot
@@ -19,25 +20,31 @@
 #     gnuplot rotation.gnuplot
 #     gnuplot force.gnuplot
 #     
-    rm stability_rad.gnuplot
-    rm stability_phi.gnuplot
-    rm stability_theta.gnuplot
-    rm vel_binned.gnuplot
-    rm vel_vs_r.gnuplot
-    rm vel_stability.gnuplot
-    rm vel_stability_phi.gnuplot
-    rm vel_stability_theta.gnuplot
-    rm pots.gnuplot
-    rm den.gnuplot
-    rm rotation.gnuplot
-    rm force.gnuplot
+#     rm stability_rad.gnuplot
+#     rm stability_phi.gnuplot
+#     rm stability_theta.gnuplot
+#     rm vel_binned.gnuplot
+#     rm vel_vs_r.gnuplot
+#     rm vel_stability.gnuplot
+#     rm vel_stability_phi.gnuplot
+#     rm vel_stability_theta.gnuplot
+#     rm pots.gnuplot
+#     rm den.gnuplot
+#     rm rotation.gnuplot
+#     rm force.gnuplot
 
     
     gnuplot stability_rad_multiplot.gnuplot
     gnuplot multiplot_phi.gnuplot
     gnuplot multiplot_theta.gnuplot
-    gnuplot stability_vel_multiplot.gnuplot
     
     gnuplot stability_vel_multiplot.gnuplot
     gnuplot multiplot_vel_theta.gnuplot
-    gnuplot  multiplot_vel_phi.gnuplot
+    gnuplot multiplot_vel_phi.gnuplot
+    
+    rm stability_rad_multiplot.gnuplot
+    rm multiplot_phi.gnuplot
+    rm multiplot_theta.gnuplot
+    rm stability_vel_multiplot.gnuplot
+    rm multiplot_vel_theta.gnuplot
+    rm multiplot_vel_phi.gnuplot
