7d9395edab2ba7c1bd7446509f174f01e914d9f6
shelts <shelts@rpi.edu>
2017-04-25 17:22:48 -0400

    added new structs pos and vel. the bodies struct now have those in it. The way functions are called are also changed so that the pos struct is sent in and not just r

--- save_scripts.sh
+++ save_scripts.sh
@@ -1,3 +1,3 @@
 git add *.cpp *.sh *.py *.h
-git commit -m "update"
+git commit -m "added new structs pos and vel. the bodies struct now have those in it. The way functions are called are also changed so that the pos struct is sent in and not just r"
 git push
